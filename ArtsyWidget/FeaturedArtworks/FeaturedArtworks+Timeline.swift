jonallured/eigen

Add the featured artworks widget

ArtsyWidget/FeaturedArtworks/FeaturedArtworks+Timeline.swift
@@ -1,0 +1,31 @@
+import WidgetKit
+
+extension FeaturedArtworks {
+    struct Timeline {
+        static func generate(completion: @escaping (WidgetKit.Timeline<Entry>) -> ()) {
+            ArtworkStore.fetch() { artworks in
+                let schedule = Schedule()
+                let rotatedArtworks = Something.rotateArray(array: artworks)
+                let updateTimesToArtworks = Array(zip(schedule.updateTimes, rotatedArtworks))
+                let entries = updateTimesToArtworks.map() { (date, artworks) in Entry(artworks: artworks, date: date) }
+                let timeline = WidgetKit.Timeline(entries: entries, policy: .after(schedule.nextUpdate))
+                completion(timeline)
+            }
+        }
+    }
+}
+
+struct Something {
+    static func rotateArray(array: [Artwork]) -> [[Artwork]] {
+        let initialLineup = array
+
+        let nextLineups: [[Artwork]] = [0, 1, 2, 3].map() { offset in
+            let trailing = Array(initialLineup.suffix(4 - offset))
+            let leading = Array(initialLineup.prefix(offset))
+
+            return trailing + leading
+        }
+        
+        return nextLineups
+    }
+}

mvpcraft/Art-Marketplace

ArtsyWidget/FeaturedArtworks/FeaturedArtworks+Timeline.swift
@@ -1,0 +1,29 @@
+import WidgetKit
+
+extension FeaturedArtworks {
+    struct Timeline {
+        static func rotateArtworks(artworks: [Artwork]) -> [[Artwork]] {
+            let initialLineup = artworks
+
+            let nextLineups: [[Artwork]] = [0, 1, 2, 3].map() { offset in
+                let trailing = Array(initialLineup.suffix(4 - offset))
+                let leading = Array(initialLineup.prefix(offset))
+
+                return trailing + leading
+            }
+            
+            return nextLineups
+        }
+        
+        static func generate(completion: @escaping (WidgetKit.Timeline<Entry>) -> ()) {
+            ArtworkStore.fetch() { artworks in
+                let schedule = Schedule()
+                let rotatedArtworks = Timeline.rotateArtworks(artworks: artworks)
+                let updateTimesToArtworks = Array(zip(schedule.updateTimes, rotatedArtworks))
+                let entries = updateTimesToArtworks.map() { (date, artworks) in Entry(artworks: artworks, date: date) }
+                let timeline = WidgetKit.Timeline(entries: entries, policy: .after(schedule.nextUpdate))
+                completion(timeline)
+            }
+        }
+    }
+}